659ef264eb7b1c5def47b7c4d81e8c16b00eb46c
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-12-20 14:54:52 +0530

    first comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-  module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-      "./node_modules/flowbite/**/*.js"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [require("@nextui-org/react"),
+const { nextui } = require("@nextui-org/react");
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui(),
     require('flowbite/plugin')
-    ],
-  }
+  ],
+}
